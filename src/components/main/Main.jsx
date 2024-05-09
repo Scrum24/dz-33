e0b87331fe8facc17799ca9e536b5c282eed12c7
Scrum24 <orlyanskiy.dmitry@gmail.com>
2024-05-10 00:07:42 +0200

    App files

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../nav/Nav";
+import Content from "../content/Content";
+import "./Main.css";
+
+function Main() {
+  return (
+    <main>
+      <Nav />
+      <Content />
+    </main>
+  );
+}
+
+export default Main;
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <nav>
+      <h3>Navigation:</h3>
+      <ul>
+        <li>
+          <a href="#">Header</a>
+        </li>
+        <li>
+          <a href="#">Content</a>
+        </li>
+        <li>
+          <a href="#">Footer</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
